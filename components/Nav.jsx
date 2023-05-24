54b5bc809d6f4c40bcdf69cd20c7c3b9abb086ec
Mohammd Mujaheed <mujaheedsami13@gmail.com>
2023-05-24 23:14:28 +0530

    Fixed: Nav Create Post error

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,9 @@ import { useEffect, useState } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react"
 
 const Nav = () => {
-    const isUserLoggedIn = true;
+    // const isUserLoggedIn = true;
+
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -14,13 +16,14 @@ const Nav = () => {
         <Image 
         src="/assets/images/logo.svg" 
         alt="AI Prompt Logo"
-        width={30}
-        height={30}
+        width="30"
+        height="30"
         className="object-contain"
         />
         <p className="logo_text">AI Prompt</p>
-        {/*Desktop Navigation*/}
-        <div className="sm:flex hidden">
+      </Link>
+      {/*Desktop Navigation*/}
+      <div className="sm:flex hidden">
             { isUserLoggedIn ? 
                 <div className="flex gap-3 md:gap-5">
                     <Link href="/create-prompt"
@@ -28,10 +31,12 @@ const Nav = () => {
                         Create Post
                     </Link>
                 </div>
-                : (<></>)
+                : 
+                (
+                <>
+                </>)
             }
         </div>
-      </Link>
     </nav>
   )
 }
